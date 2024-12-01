357f50039f3ad81c504668a7230bc42d6076313b
JairAlexey <alexeyrueda823@gmail.com>
2024-12-01 13:49:46 -0500

    Configuración inicial del CI/CD

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 export default {
     testEnvironment: 'node',
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
+    transform: {},
     extensionsToTreatAsEsm: ['.js'],
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1'
-    },
-    testMatch: ['**/__tests__/**/*.js'],
-    verbose: true
+    }
 };
